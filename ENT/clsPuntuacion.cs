f5c4e9317fe958c816f404a969cda681dcf49ed1
dvi26 <145675318+dvi26@users.noreply.github.com>
2025-05-26 13:36:08 +0200

    Agregar archivos de proyecto.

---
+++ ENT/clsPuntuacion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class clsPuntuacion
+    {
+        #region Atributos
+        private int id;
+        private string nombreJugador;
+        private int puntuacion;
+        #endregion
+
+        #region Propiedades
+        public int Id
+        {
+            get { return id; }
+        }
+        public string NombreJugador
+        {
+            get { return nombreJugador; }
+            set { nombreJugador = value; }
+        }
+        public int Puntuacion
+        {
+            get { return puntuacion; }
+            set { puntuacion = value; }
+        }
+        #endregion
+
+        #region Constructores
+        public clsPuntuacion(int id, string nombreJugador, int puntuacion)
+        {
+            this.id = id;
+            this.nombreJugador = nombreJugador;
+            this.puntuacion = puntuacion;
+        }
+        public clsPuntuacion()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Services/ManejadoraPuntuacionApi.cs
@@ -0,0 +1,92 @@
+﻿using ENT;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ManejadoraPuntuacionApi
+    {
+        private static string urlBase = "https://apipuntuacionpkm.azurewebsites.net/Api/Puntuacion";
+
+        /// <summary>
+        /// Obtiene un listado completo de puntuaciones desde la API ordenado de mayor a menor
+        /// </summary>
+        /// <returns>Lista de puntuaciones</returns>
+        public static async Task<List<clsPuntuacion>> getListadoPuntuacionessAsync()
+        {
+            Uri requestUri = new Uri($"{urlBase}");
+
+            List<clsPuntuacion> puntuaciones = new List<clsPuntuacion>();
+
+  
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(requestUri);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        puntuaciones = JsonConvert.DeserializeObject<List<clsPuntuacion>>(jsonResponse);
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        throw new Exception("404");
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+                        throw new Exception("400");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error al obtener la lista de puntuaciones", ex);
+                }
+            }
+
+            return puntuaciones;
+        }
+        /// <summary>
+        /// Añade una nueva puntuacion a través de la API
+        /// </summary>
+        /// <param name="nuevaPuntuacion"></param>
+        /// <returns>Código de estado HTTP de la respuesta</returns>
+        public static async Task<int> AgregarPersonaAsync(clsPuntuacion nuevaPuntuacion)
+        {
+            Uri requestUri = new Uri($"{urlBase}");
+            int filasAfectadas = 0;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    string jsonData = JsonConvert.SerializeObject(nuevaPuntuacion);
+                    HttpContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await httpClient.PostAsync(requestUri, content);
+                    filasAfectadas = (int)response.StatusCode;
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        throw new Exception("404");
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+                        throw new Exception("400");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error al agregar la persona", ex);
+                }
+                return filasAfectadas;
+            }
+        }
+    }
+}
